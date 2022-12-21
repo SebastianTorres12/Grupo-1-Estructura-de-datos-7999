006ddbf5989ca53d9961ba127a450baba49c2261
PaolaMoncayo <87495121+PaolaMoncayo@users.noreply.github.com>
2022-12-21 07:10:33 -0500

    Update Fecha.h

--- Tarea2_GestionColaBanco/Fecha.h
+++ Tarea2_GestionColaBanco/Fecha.h
@@ -1,3 +1,12 @@
+/***
+UFA - ESPE
+AUTORES: Matías Padrón,Sebastian Torres, Cristhoper Villamarin, Paola Moncayo, Camilo Orrico, Jeimy Morales, Ariel Guevera
+FECHA DE CREACIÓN: 19/12/2022
+FECHA DE MODIFICACIÓN: 21/12/2022
+Grupo1-Gestión banco usando Colas
+GITHUB: Grupo-1-Estructura-de-datos-7999
+ ***/
+
 /***********************************************************************
  * Module:  Fecha.h
  * Author:  sebas
