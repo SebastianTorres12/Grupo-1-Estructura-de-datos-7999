e5141f57ef23c076a77343f56db03c677ab45b3d
Camilo Andrés Orrico Emme <99036443+caorrico@users.noreply.github.com>
2022-12-21 02:36:15 -0500

    ACTUALIZO

--- Tarea2_GestionColaBanco/NodoSimple.cpp
+++ Tarea2_GestionColaBanco/NodoSimple.cpp
@@ -1,11 +1,11 @@
-/*************************
+/*********
 UFA - ESPE
-AUTORES: MatÃ­as PadrÃ³n,Sebastian Torres, Cristhoper Villamarin, Paola Moncayo, Camilo Orrico, Jeimy Morales, Ariel Guevera
-FECHA DE CREACIÃ“N: 19/12/2022
-FECHA DE MODIFICACIÃ“N: 19/12/2022
-Grupo1-GestiÃ³n banco usando Colas
+AUTORES: Matías Padrón,Sebastian Torres, Cristhoper Villamarin, Paola Moncayo, Camilo Orrico, Jeimy Morales, Ariel Guevera
+FECHA DE CREACIÓN: 19/12/2022
+FECHA DE MODIFICACIÓN: 21/12/2022
+Grupo1-Gestión banco usando Colas
 GITHUB: Grupo-1-Estructura-de-datos-7999
- *************************/
+ *********/
 
 #include "NodoSimple.h"
 
