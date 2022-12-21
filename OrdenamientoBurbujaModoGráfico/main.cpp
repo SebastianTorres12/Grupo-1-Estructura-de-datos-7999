08e6acee45295b427c3666f58b7226b635045b2b
Jeimy <jeimymorales234@gmail.com>
2022-12-21 02:18:59 -0500

    Banco

--- OrdenamientoBurbujaModoGráfico/main.cpp
+++ OrdenamientoBurbujaModoGráfico/main.cpp
@@ -18,37 +18,37 @@ using namespace std;
 
 int main()
 {
-Burbuja b;
-ValidacionDatos objValidar;
-char elementosV[10];
-initwindow(1150,500); 
-char vecS[10];
-int numero=0;
-settextstyle(7,HORIZ_DIR,6);
-outtextxy(350,1,"Metodo burbuja");
-settextstyle(7,HORIZ_DIR,2);
-setcolor(15);
-do{
-	outtextxy(350,270,"     ");
-	outtextxy(350,250,"Ingrese la cantidad de numeros:");
-	cout<<"\nIngrese la cantidad de elementos del arreglo a ordenar: "<<endl;
-	cout<<"Maximo 15 elementos"<<endl;
-	cin>>numero;
-	sprintf(vecS,"%d",numero);
-	outtextxy(350,270,vecS);
-	delay(1000);
-}while(numero>15);
+	Burbuja b;
+	ValidacionDatos objValidar;
+	char elementosV[10];
+	initwindow(1150,500); 
+	char vecS[10];
+	int numero=0;
+	settextstyle(7,HORIZ_DIR,6);
+	outtextxy(350,1,"Metodo burbuja");
+	settextstyle(7,HORIZ_DIR,2);
+	setcolor(15);
+	do{ 
+		outtextxy(350,270,"     ");
+		outtextxy(350,250,"Ingrese la cantidad de numeros:");
+		cout<<"\nIngrese la cantidad de elementos del arreglo a ordenar: "<<endl;
+		cout<<"Maximo 15 elementos"<<endl;
+		cin>>numero;
+		sprintf(vecS,"%d",numero);
+		outtextxy(350,270,vecS);
+		delay(1000);
+	}while(numero>15 );
 	outtextxy(390,270," Valido");
-int arr[numero];
-for (int i = 0; i < numero; i++) {
-    arr[i] = 10+rand()%(100-10);
-  }
-
-b.dibujarCuadros(arr, numero, -1, -1);
-delay(1200);
-b.burbujaGrafico(arr, numero);
-
-getch();
-closegraph();
-return 0;
+	int arr[numero];
+	for (int i = 0; i < numero; i++) {
+	    arr[i] = 10+rand()%(100-10);
+	}
+	
+	b.dibujarCuadros(arr, numero, -1, -1);
+	delay(1200);
+	b.burbujaGrafico(arr, numero);
+	
+	getch();
+	closegraph();
+	return 0;
 }
