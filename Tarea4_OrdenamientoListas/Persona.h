0f5e72d312b7fba4430aea5a6e70e18aea64c03f
Cristhopher-Villamarin <87491312+Cristhopher-Villamarin@users.noreply.github.com>
2023-01-04 03:04:42 -0500

    Update Persona.h

--- Tarea4_OrdenamientoListas/Persona.h
+++ Tarea4_OrdenamientoListas/Persona.h
@@ -1,1 +1,35 @@
-a
+/*************************
+UFA - ESPE
+AUTORES: Matías Padrón,Sebastian Torres, Cristhoper Villamarin, Paola Moncayo, Camilo Orrico, Jeimy Morales, Ariel Guevera
+FECHA DE CREACIÓN: 20/12/2022
+FECHA DE MODIFICACIÓN: 20/12/2022
+Grupo1-Métodos de búsqueda 
+GITHUB: Grupo-1-Estructura-de-datos-7999
+ *************************/
+
+#if !defined(__Class_Diagram_1_Persona_h)
+#define __Class_Diagram_1_Persona_h
+
+#include <string>
+#include <stdlib.h>
+#pragma once
+class Persona 
+{
+public:
+   Persona(std::string ced,std::string nom,std::string apell);
+   ~Persona();
+   std::string getCedula(void);
+   void setCedula(std::string newCedula);
+   std::string getNombre(void);
+   void setNombre(std::string newNombre);
+   std::string getApellido(void);
+   void setApellido(std::string newApellido);
+ 
+protected:
+private:
+   std::string cedula;
+   std::string nombre;
+   std::string apellido;   
+};
+
+#endif
