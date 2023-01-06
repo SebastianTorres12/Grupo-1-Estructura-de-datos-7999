491db320e99d2ad4a013bce70b4a9f6ee51cbd3d
SebastianTorres12 <105263296+SebastianTorres12@users.noreply.github.com>
2023-01-05 23:50:24 -0500

    Update Nodo.cpp

--- Tarea5_Notaciones/Nodo.cpp
+++ Tarea5_Notaciones/Nodo.cpp
@@ -1,3 +1,12 @@
+/*************************
+UFA - ESPE
+AUTORES: Matías Padrón,Sebastian Torres, Cristhoper Villamarin, Paola Moncayo, Camilo Orrico, Jeimy Morales, Ariel Guevera
+FECHA DE CREACIÓN: 01/01/2022
+FECHA DE MODIFICACIÓN: 05/01/2022
+Grupo1-Notaciones
+GITHUB: Grupo-1-Estructura-de-datos-7999
+ *************************/
+
 #include"Nodo.h"
 
 Nodo::Nodo(string _valor, Nodo *_siguiente)
