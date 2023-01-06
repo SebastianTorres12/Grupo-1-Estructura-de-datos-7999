e373ab2c61a5eec49b121c34a1dfca9b31f7f2d1
PaolaMoncayo <87495121+PaolaMoncayo@users.noreply.github.com>
2023-01-05 23:51:14 -0500

    Update Menu.h

--- Tarea5_Notaciones/Menu.h
+++ Tarea5_Notaciones/Menu.h
@@ -1,11 +1,11 @@
-/*************************
+/*********
 UFA - ESPE
 AUTORES: Matías Padrón,Sebastian Torres, Cristhoper Villamarin, Paola Moncayo, Camilo Orrico, Jeimy Morales, Ariel Guevera
-FECHA DE CREACIÓN: 20/12/2022
-FECHA DE MODIFICACIÓN: 20/12/2022
-Grupo1-Métodos de búsqueda 
+FECHA DE CREACIÓN: 01/01/2022
+FECHA DE MODIFICACIÓN: 05/01/2022
+Grupo1-Notaciones
 GITHUB: Grupo-1-Estructura-de-datos-7999
- *************************/
+ *********/
 
 
  #include <iostream>
